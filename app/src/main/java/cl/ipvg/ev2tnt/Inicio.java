b10683d91f50f5effaf066bfc578cbe1b2d672d2
Alumnos <Alumnos@L203-EQUIPO24>
2023-10-12 16:04:28 -0300

    Se agregó intents para la pantalla de conductor

--- app/src/main/java/cl/ipvg/ev2tnt/Inicio.java
+++ app/src/main/java/cl/ipvg/ev2tnt/Inicio.java
@@ -2,13 +2,31 @@ package cl.ipvg.ev2tnt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
-
+    Button btUser;
+    Button btDriver;
+    Intent intent1;
+    Intent intent2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        intent1 = new Intent(this, MainActivity.class);
+
+        btUser = (Button) findViewById(R.id.btUsuario);
+        btDriver = (Button) findViewById(R.id.btConductor);
+
+        btDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent1);
+            }
+        });
     }
 }
