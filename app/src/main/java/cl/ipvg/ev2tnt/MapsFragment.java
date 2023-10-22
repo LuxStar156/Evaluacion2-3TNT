7e3b622cc145614bb617c71490a45ccbb57d3768
LuxStar156 <130090069+LuxStar156@users.noreply.github.com>
2023-10-21 22:53:20 -0300

    Corrección de errores 1.2

--- app/src/main/java/cl/ipvg/ev2tnt/MapsFragment.java
+++ app/src/main/java/cl/ipvg/ev2tnt/MapsFragment.java
@@ -3,7 +3,7 @@ package cl.ipvg.ev2tnt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
+
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,20 +17,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import cl.ipvg.ev2tnt.Scripts.LeerDatos;
 
 
 public class MapsFragment extends Fragment {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LeerDatos leerDatos = new LeerDatos();
-            leerDatos.listarC();
 
-            LatLng vehiculo= new LatLng(leerDatos.getLatitud(), leerDatos.getLongitud());
+
+            LatLng vehiculo= new LatLng(40.423, -122.1223);
             googleMap.addMarker(new MarkerOptions().position(vehiculo).title("tu locomocion!!!"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(vehiculo));
         }
