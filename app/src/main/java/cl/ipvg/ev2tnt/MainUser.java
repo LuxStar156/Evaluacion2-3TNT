edf3a7d6bc9c8de9afd408df5fdff6c96f88f035
LuxStar156 <130090069+LuxStar156@users.noreply.github.com>
2023-11-12 01:03:15 -0300

    listas v 2.7

--- app/src/main/java/cl/ipvg/ev2tnt/MainUser.java
+++ app/src/main/java/cl/ipvg/ev2tnt/MainUser.java
@@ -2,28 +2,10 @@ package cl.ipvg.ev2tnt;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.ChildEventListener;
@@ -33,10 +15,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+
 
 import cl.ipvg.ev2tnt.Clases.Vehiculo;
 
@@ -48,7 +29,6 @@ public class MainUser extends AppCompatActivity {
     Double lon;
     private List<Vehiculo> Listvehiculo = new ArrayList<Vehiculo>();
     private List<String> ListVehiculoNombre = new ArrayList();
-    ArrayAdapter<Vehiculo> arrayAdapterLibro;
     ArrayAdapter<String> arrayAdapterString;
 
     @Override
@@ -57,7 +37,7 @@ public class MainUser extends AppCompatActivity {
         setContentView(R.layout.activity_main_user);
 
         lvehiculo = (ListView) findViewById(R.id.tvListado);
-
+        inicializarFireBase();
         enviarDatos();
 
         MapsFragment mapfragment = new MapsFragment();
@@ -66,18 +46,15 @@ public class MainUser extends AppCompatActivity {
     }
 
     public void enviarDatos(){
-        FirebaseApp.initializeApp(this);
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference =firebaseDatabase.getReference();
 
-        databaseReference.child("Libro").addValueEventListener(new ValueEventListener() {
+        databaseReference.child("Vehiculo").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Listvehiculo.clear();
                 for (DataSnapshot objs : snapshot.getChildren()){
                     Vehiculo li =objs.getValue(Vehiculo.class);
                     Listvehiculo.add(li);
-                    ListVehiculoNombre.add(""+li.getNombre()+" "+li.getApellido());
+                    ListVehiculoNombre.add(""+li.getNombre()+" "+li.getApellido()+" "+li.getLinea());
                     arrayAdapterString =new ArrayAdapter<String>(MainUser.this, android.R.layout.simple_expandable_list_item_1,ListVehiculoNombre);
                     lvehiculo.setAdapter(arrayAdapterString);
                 }
@@ -89,5 +66,11 @@ public class MainUser extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void inicializarFireBase(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase =FirebaseDatabase.getInstance();
+        databaseReference =firebaseDatabase.getReference();
     }
 }
